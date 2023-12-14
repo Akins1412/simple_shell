74052e6c95890bdccdd114c882415567ee56006e
Emmanuel <138694714+Akins1412@users.noreply.github.com>
2023-12-14 22:17:52 +0000

    Update shell.h

--- shell.h
+++ shell.h
@@ -60,7 +60,7 @@ void execute_setenv(shell_vars *vars);
 void execute_unsetenv(shell_vars *vars);
 
 char **tokenize_input(char *input_buffer, char *delimiter);
-char **resize_array(char **array_ptr, size_t *size);
+char **custom_reallocate(char **point, size_t *size);
 char *custom_strtok(char *str, const char *delim);
 
 void display_error(shell_vars *shell_vars, char *message);
