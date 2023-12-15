b91c5f66e40d7de6e8356bfbd05c8dc703de4ec7
Emmanuel <138694714+Akins1412@users.noreply.github.com>
2023-12-15 02:13:17 +0000

    Update and rename strngfunc.c to strngfunc44.c

--- strngfunc44.c
+++ strngfunc44.c
@@ -21,53 +21,6 @@ ssize_t print_string(char *str)
 }
 
 /**
- * duplicate_string - Creates a duplicate of the
- * provided string in a new memory
- * @str_to_duplicate: String to be duplicated
- *
- * Return: Pointer to the new duplicated string
- */
-char *duplicate_string(char *str_to_duplicate)
-{
-	char *ct;
-
-	int l, i;
-
-	if (str_to_duplicate == 0)
-		return (NULL);
-
-	for (l = 0; str_to_duplicate[l]; l++)
-		;
-	ct = malloc((l + 1) * sizeof(char));
-
-	for (i = 0; i <= l; i++)
-		ct[i] = str_to_duplicate[i];
-
-	return (ct);
-}
-
-/**
- * compare_strings - Compares the lengths of two strings
- * @str1: First string to be compared in length
- * @str2: Second string to be compared
- *
- * Return: 0 on success, any other value indicates a failure
- */
-int compare_strings(char *str1, char *str2)
-{
-	int i;
-
-	i = 0;
-	while (str1[i] == str2[i])
-	{
-		if (str1[i] == '\0')
-			return (0);
-		i++;
-	}
-	return (str1[i] - str2[i]);
-}
-
-/**
  * concatenate_strings - Concatenates two strings
  * @str1: First string
  * @str2: Second string
